33641fe999958556096afbbbdcf0b2a5f15bd18e
Godsenal <tmqps78@gmail.com>
2017-03-13 17:06:03 +0900

    hehe

--- build/config.js
+++ build/config.js
@@ -11,7 +11,7 @@ exports.default = {
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
   get serverUrl() {
-    return 'http://' + this.host + ':' + this.port;
+    return 'https://' + this.host + ':' + this.port;
   },
   //dbUrl : 'mongodb://localhost:27017/dbTest',
   dbUrl: 'mongodb://godsenal:xogmltjdrhd77@ds129600.mlab.com:29600/heroku_k4k7hk8l'
--- server/config.js
+++ server/config.js
@@ -7,7 +7,7 @@ export default {
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
   get serverUrl() {
-    return `http://${this.host}:${this.port}`;
+    return `https://${this.host}:${this.port}`;
   },
   //dbUrl : 'mongodb://localhost:27017/dbTest',
   dbUrl : 'mongodb://godsenal:xogmltjdrhd77@ds129600.mlab.com:29600/heroku_k4k7hk8l'
