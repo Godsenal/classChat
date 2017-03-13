48495e2973f445830d3bfa4404c25ce98741353e
Godsenal <tmqps78@gmail.com>
2017-03-13 13:12:26 +0900

    port change

--- server/config.js
+++ server/config.js
@@ -4,8 +4,8 @@ export const nodeEnv = env.NODE_ENV || 'development';
 
 
 export default {
-  port: env.OPENSHIFT_NODEJS_PORT || 8080,
-  host: env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  port: env.PORT || 8080,
+  host: env.HOST || '0.0.0.0',
   get serverUrl() {
     return `http://${this.host}:${this.port}`;
   },
