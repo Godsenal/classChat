12228e6ed66447944c2a8ce9408bb503cde2d89c
godsenal <tmqps78@gmail.com>
2018-09-05 11:17:32 +0900

    Update config private value

--- server/config.js
+++ server/config.js
@@ -1,6 +1,5 @@
 const env = process.env;
 const localDB = 'mongodb://localhost:27017/chatDB';
-const cloudDB = 'mongodb://godsenal:xogmltjdrhd77@ds129600.mlab.com:29600/heroku_k4k7hk8l';
 export const nodeEnv = env.NODE_ENV || 'development';
 
 
